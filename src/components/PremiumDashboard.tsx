350532f8dc8d0de482327c428eecdbb7585bb047
[dyad] <plano7nutri@gmail.com>
2026-05-04 11:02:34 -0300

    [dyad] Espaçamentos reduzidos no Dashboard Pago. - wrote 1 file(s)

--- src/components/PremiumDashboard.tsx
+++ src/components/PremiumDashboard.tsx
@@ -571,7 +571,7 @@ const PremiumDashboard = ({
           <motion.div 
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
-            className="mt-16 pt-16 border-t border-emerald-500/10"
+            className="mt-8 pt-8 border-t border-emerald-500/10"
           >
             <PricingSection isDark={true} />
           </motion.div>
