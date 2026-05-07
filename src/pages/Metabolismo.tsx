67290589f6ac889e5397c4f71d284890f7ef2ae1
[dyad] <plano7nutri@gmail.com>
2026-05-07 12:28:36 -0300

    [dyad] Correção do link do botão para /login mantendo o texto original. - wrote 1 file(s)

--- src/pages/Metabolismo.tsx
+++ src/pages/Metabolismo.tsx
@@ -41,7 +41,7 @@ const Metabolismo = () => {
           hideFree={true}
           reverseSections={true}
           startBtnText="Quero Meu Plano Nutricional"
-          loginBtnText="Login Plano Pago"
+          loginBtnText="Acessar Meu Plano"
         />
       </main>
       <Footer />
